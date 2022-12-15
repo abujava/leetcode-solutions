1dadec8325d684147e19123975b73a6a2195caaf
Muhammad <shaymanov.2000@gmail.com>
2022-12-15 22:13:10 +0500

    115. Distinct Subsequences

--- com/abujava/Main.java
+++ com/abujava/Main.java
@@ -2,7 +2,7 @@ package com.abujava;
 
 import java.util.Random;
 
-import static com.abujava.Strings.numJewelsInStones;
+import static com.abujava.Strings.numDistinct2;
 
 /**
  * This class not documented :(
@@ -15,7 +15,7 @@ public class Main {
         int[][] matrix = buildMatrix(4, 4);
 
 
-        var i = numJewelsInStones("aA", "aAAbbbb");
+        var i = numDistinct2("rabbbit", "rabbit");
         System.out.println(i);
     }
 
@@ -28,7 +28,19 @@ public class Main {
             }
             System.out.println();
         }
+        System.out.println();
         return matrix;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
     }
 
 }
--- com/abujava/Strings.java
+++ com/abujava/Strings.java
@@ -7,6 +7,80 @@ package com.abujava;
  * @since 12/15/2022
  */
 public class Strings {
+    /**
+     * Hard <a href="https://leetcode.com/problems/distinct-subsequences">115. Distinct Subsequences</a>
+     */
+    public static int numDistinct(String s, String t) {
+        if (t.length() > s.length())
+            return 0;
+
+        int[] tArr = new int[t.length()];
+        for (int i = 0; i < tArr.length; i++) {
+            tArr[i] = i;
+        }
+
+        int sum = 0;
+
+        while (true) {
+            String text = "";
+            for (int i = 0; i < tArr.length; i++) {
+                text += s.charAt(tArr[i]);
+            }
+            if (text.equals(t)) {
+                sum++;
+            }
+
+            for (int i = tArr.length - 1; i >= 0; i--) {
+                if (tArr[i] != ((s.length()) - ((tArr.length) - i))) {
+                    tArr[i]++;
+                    break;
+                } else if (i == 0) {
+                    return sum;
+                }
+            }
+        }
+    }
+
+    /**
+     * Hard <a href="https://leetcode.com/problems/distinct-subsequences">115. Distinct Subsequences</a>
+     */
+    public static int numDistinct2(String s, String t) {
+        if (s == null || t == null) {
+            if (s == null && t == null) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        if (s.length() == 0 && t.length() == 0) {
+            return 1;
+        }
+
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+        for (int i = 0; i < s.length(); i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= i && j <= t.length(); j++) {
+                Main.printMatrix(dp);
+                if (s.charAt(i - 1) != t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[s.length()][t.length()];
+    }
+
+    /**
+     * Medium <a href="https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/">28. Find the Index of the First Occurrence in a String</a>
+     */
+    public static int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+
     public static int numJewelsInStones(String jewels, String stones) {
         int result = 0;
         for (int i = 0; i < stones.length(); i++) {
