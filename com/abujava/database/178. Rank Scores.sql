db463d1d3cce5d2b0c655f55ae76ecb3bd956a16
Muhammad <shaymanov.2000@gmail.com>
2022-12-16 19:33:30 +0500

    MySQL

---
+++ com/abujava/database/178. Rank Scores.sql
@@ -0,0 +1,13 @@
+-- 178. Rank Scores - https://leetcode.com/problems/rank-scores/
+
+Create table If Not Exists Scores (id int, score DECIMAL(3,2));
+Truncate table Scores;
+insert into Scores (id, score) values ('1', '3.5');
+insert into Scores (id, score) values ('2', '3.65');
+insert into Scores (id, score) values ('3', '4.0');
+insert into Scores (id, score) values ('4', '3.85');
+insert into Scores (id, score) values ('5', '4.0');
+insert into Scores (id, score) values ('6', '3.65');
+
+-- solution
+select score, row_number() over (order by score desc) as rank from scores;
